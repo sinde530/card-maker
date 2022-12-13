ce12f408a63b947fed259a493c1eca8c0aabdf62
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-12-13 17:38:34 +0900

    emotion/styled theme test

--- src/components/Preview/PreviewCard.tsx
+++ src/components/Preview/PreviewCard.tsx
@@ -1,9 +1,80 @@
+import styled from "@emotion/styled";
 import { Card } from "src/types/Card";
 
 interface Prop {
   card: Card;
 }
 
+interface ContainerProps {
+  theme: Card["theme"];
+}
+interface MetaDataProps {
+  theme: Card["theme"];
+}
+interface DataProps {
+  $name?: boolean;
+}
+
+const Container = styled.li<ContainerProps>`
+  display: flex;
+  border: 1px solid black;
+  border-radius: 1rem;
+  margin: 0.5em 3em;
+  padding: 1.2em;
+  background: ${(props) => {
+    switch (props.theme) {
+      case "light":
+        return "white";
+      case "dark":
+        return "black";
+      case "colorful":
+        return "linear-gradient(45deg, Violet, Orange)";
+      default:
+        return "white";
+    }
+  }};
+  width: 100%;
+  max-width: 35rem;
+`;
+
+const Image = styled.img`
+  flex-basis: 15%;
+  width: 8rem;
+  height: 8rem;
+  margin-right: 1.5em;
+  border-radius: 50%;
+`;
+const MetaData = styled.div<MetaDataProps>`
+  display: flex;
+  flex-basis: 85%;
+  flex-direction: column;
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
+`;
+const Data = styled.span<DataProps>`
+  font-size: ${(props) => (props.$name ? "1.8rem" : "1.2rem")};
+  text-align: left;
+  &:nth-child(2)::after {
+    content: "";
+    display: block;
+    width: 90%;
+    height: 1px;
+    background-color: lightgray;
+  }
+`;
+
 export default function PreviewCard({ card }: Prop) {
-  return <div>PreviewCard</div>;
+  const { id, name, company, theme, title, email, message, fileUrl } = card;
+
+  return (
+    <Container theme={theme}>
+      <Image src="" alt="" />
+      <MetaData theme={theme}>
+        <Data $name>{name}</Data>
+        <Data>{company}</Data>
+        <Data>{title}</Data>
+        <Data>{email}</Data>
+        <Data>{message}</Data>
+      </MetaData>
+    </Container>
+  );
 }
--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Card } from "src/types/Card";
 import PreviewCard from "./PreviewCard";
 
@@ -5,15 +6,39 @@ interface Prop {
   cards: Card[];
 }
 
+const EditorContainer = styled.div({
+  textAlign: "center",
+  flexBasis: "50%",
+  overflowY: "auto",
+});
+
+const TitleText = styled.h1({
+  fontSize: "24px",
+  position: "sticky",
+  top: "0px",
+  padding: "8px",
+  marginTop: "0",
+  backgroundColor: "wheat",
+  color: "#000000",
+});
+
+const Ul = styled.ul({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "0",
+  paddingLeft: "0",
+  width: "100%",
+});
+
 export default function Preview({ cards }: Prop) {
   return (
-    <div>
-      <h1>Card Preview</h1>
-      <ul>
+    <EditorContainer>
+      <TitleText>Card Preview</TitleText>
+      <Ul>
         {cards.map((card) => (
           <PreviewCard card={card} key={card.id} />
         ))}
-      </ul>
-    </div>
+      </Ul>
+    </EditorContainer>
   );
 }
